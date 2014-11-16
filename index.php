bf27d45ccf3f6d5520bdee3d23911701513ef9cd
Seandon Mooy <admin@erulabs.com>
2014-11-15 22:09:06 -0600

    initial stuff

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+  	<header>
+  		Welcome to the LeagueSite
+  	</header>
+  	<section id="nameInput">
+  		<form method="post" action="index.php">
+        <input type="text">
+        <input type="submit">
+  		</form>
+  	</section>
+    <script async="async" src="script.js" type="text/javascript"></script>
+  </body>
+</html>
