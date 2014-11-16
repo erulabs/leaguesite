eb0573618957241aadb26399a935fc9007645252
Seandon Mooy <admin@erulabs.com>
2014-11-15 23:56:34 -0600

    adding docker so we can all work on the app locally

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@
   </head>
   <body>
   	<header>
-  		Welcome to the LeagueSite
+  		Welcome to the LeagueSite - docker is awesome
   	</header>
   	<section id="nameInput">
   		<form method="post" action="index.php">
